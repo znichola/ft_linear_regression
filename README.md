e0573fe207ca92c70488ca6eeba1b1675e710d2a
Nicholas Zivkovic <niki.zivkovic@hotmail.com>
2025-06-20 11:27:13 +0200

    predictor program with error checks in user input

--- README.md
+++ README.md
@@ -5,4 +5,39 @@ An introduction to machine learning.
 > For this project, you will have to create a program that predicts the price
 of a car by using a linear function train with a gradient descent algorithm
 
+## Predictor program
+
+```
+python predictor.py
+```
+
+This should predict the price of a car based on weights that have been
+pretrained. The program will prompt the user for the millage and return 
+the estimated price.
+
+```
+estimatedPrice(mileage) = t0 + (t1 * mileage)
+```
+
+Before training `t0` and `t1` are set to 0, training will find the best fit
+values to use.
+
+## Trainer program
+
+```
+python trainer.py
+```
+
+This will train the model to find the best values of for t0 and t1. The 
+downloaded data will be used for the training, it's a 24 entry table of 
+km & price.
+
+```
+t0 = learningRate * averageError
+t1 = learningRate * averageWeightedError
+```
+
+_weighted by millage (the single feature in this case)_
+
+This scales the eror by how much impact the millage will have on it.
 
--- predictor.py
+++ predictor.py
@@ -1,2 +1,53 @@
-print("Let's predict the price of a car!")
 
+def estimatePrice(mileage: float, t0: float, t1: float) -> float:
+    return t0 + t1 * mileage
+
+
+def is_number(s: str) -> bool:
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+def load_weights(file_path="weights.txt") -> tuple[float | None, float | None]:
+    try:
+        with open(file_path, "r") as file:
+            lines: list[str] = file.readlines()
+            if len(lines) < 2:
+                print(
+                    f"Error: {file_path} should contain two lines (t0 and t1)")
+                return None, None
+
+            t0_str: str = lines[0].strip()
+            t1_str: str = lines[1].strip()
+
+            if not is_number(t0_str) or not is_number(t1_str):
+                print("Error: t0 and t1 must be numbers.")
+                return None, None
+
+            return float(t0_str), float(t1_str)
+    except OSError:
+        print("Error: weights.txt is inaccessible.")
+        return None, None
+
+
+if __name__ == "__main__":
+    print("Let's predict the price of a car!")
+    print()
+
+    t0: float | None
+    t1: float | None
+    t0, t1 = load_weights()
+    if t0 is None or t1 is None:
+        exit(1)
+
+    mileage_input: str = input("Enter the car's mileage: ").strip()
+    if not is_number(mileage_input):
+        print("Error: Mileage must be a number.")
+        exit(1)
+
+    mileage: float = float(mileage_input)
+    estimated_price: float = estimatePrice(mileage, t0, t1)
+    print(f"Estimated car at {mileage}km: {estimated_price:.2f}")
